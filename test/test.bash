86f32bf270591746c163f7c2d435ff0035d99de3
Ruka Ishikawa <ishikawaruka.1225@gmail.com>
2026-01-18 15:36:03 +0900

    Add tests for invalid input cases

--- test/test.bash
+++ test/test.bash
@@ -4,6 +4,15 @@
 
 set -e
 
+# 正常系
 echo -e "1 2\n3 4" | ./bin/colstat | grep -q "^2 4"
-echo -e "1 2\n3 4" | ./bin/colstat | grep -q "^2 6"
+
+# 数値でない
+! echo "1 a 3" | ./bin/colstat
+
+# 列数不一致
+! echo -e "1 2\n3" | ./bin/colstat
+
+# 空入力
+! echo "" | ./bin/colstat
 
